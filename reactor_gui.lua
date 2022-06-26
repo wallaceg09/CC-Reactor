1d3f19107cfb50fcfb96f86e1bc96977672a941c
Glen Wallace <wallaceglenw@gmail.com>
2022-06-26 18:42:35 -0500

    WIP cleanup Temperature UI

--- reactor_gui.lua
+++ reactor_gui.lua
@@ -2,40 +2,72 @@ os.loadAPI("txUI")
 require("reactor")
 
 local w, h = term.getSize()
-local window = txUI.Window:new({w=w, h=h;})
+local window = txUI.Window:new({ w = w, h = h; })
 
 local reactor = peripheral.find("fissionReactorLogicAdapter")
 
 txUI.Controller:addWindow(window)
 txUI.Controller.prototype.useNative = false
 
-window:setTitleLabel(txUI.Label:new({text="Demo"; textColor=colors.white; textAlign="right"}))
-window:addComponent(txUI.Button:new({x=1; y=1; w=1; h=1; action=(function(self) self.parent:close() end); textColor=colors.red; bgColor=window.tlColor; text="X"}))
+window:setTitleLabel(txUI.Label:new({ text = "Demo"; textColor = colors.white; textAlign = "right" }))
+window:addComponent(txUI.Button:new({ x = 1; y = 1; w = 1; h = 1; action = (function(self)
+    self.parent:close()
+end); textColor = colors.red; bgColor = window.tlColor; text = "X" }))
 
-window:addComponent(txUI.Label:new({x=1; y=2; w=w; text="Temp:"; bgColor=colors.lightGray, textAlign="left"}))
-
-local temp_progress = txUI.ProgressBar:new({x=7, y=2, w=5})
-local temp_value = txUI.Label:new({x=13, y=2, text="?", textAlign="left", bgColor=colors.lightGray})
-
-window:addComponent(temp_progress)
-window:addComponent(temp_value)
-
-local dbg = txUI.Label:new({x=1; y=10; w=w; text="Debug", textAlign="left"})
+local dbg = txUI.Label:new({ x = 1; y = 10; w = w; text = "Debug", textAlign = "left" })
 window:addComponent(dbg)
 
-function app_update(self, eventTbl)
-    temp_progress.val = 100
-    reactor_dat = poll_reactor(reactor)
+temp_tbl = setup_temp()
+
+function setup_temp()
+    window:addComponent(txUI.Label:new(
+            { x = 1;
+              y = 2;
+              w = w;
+              text = "Temp:";
+              bgColor = colors.lightGray;
+              textAlign = "left"
+            })
+    )
+
+    local temp_progress = txUI.ProgressBar:new(
+            { x = 7,
+              y = 2,
+              w = 5
+            }
+    )
+    local temp_value = txUI.Label:new(
+            {
+                x = 13,
+                y = 2,
+                text = "?",
+                textAlign = "left",
+                bgColor = colors.lightGray
+            }
+    )
+
+    window:addComponent(temp_progress)
+    window:addComponent(temp_value)
+
+    return { progress = temp_progress, value_label = temp_value }
+end
+
+function update_temp(reactor_dat)
     temp_perc_100 = math.floor((reactor_dat.temp / reactor_dat.max_temp * 100) + 0.5)
     temp_perc_100 = math.min(temp_perc_100, 100)
 
-    temp_progress.val = temp_perc_100
+    temp_tbl.progress.val = temp_perc_100
 
     temp_celcius = reactor_dat.temp - 273.15
-    temp_value.text=string.format("%dc", temp_celcius)
+    temp_tbl.value_label.text = string.format("%dc", temp_celcius)
+end
+
+function app_update(self, eventTbl)
+    reactor_dat = poll_reactor(reactor)
+    update_temp(reactor_dat)
 
     --    dbg.text=string.format("%d/%d", reactor_dat.temp, reactor_dat.max_temp)
-    dbg.text=eventTbl[1]
+    dbg.text = eventTbl[1]
 end
 
 txUI.Controller.prototype.appUpdate = app_update
