688f2f6a08d12b0d865eddece545818aeecb4b5a
Glen Wallace <wallaceglenw@gmail.com>
2022-06-28 20:00:14 -0500

    Fix reactor_watcher percentages
    Add heated coolant

--- reactor_watcher.lua
+++ reactor_watcher.lua
@@ -2,33 +2,43 @@ require("reactor")
 
 local reactor = get_reactor()
 
-local max_coolant_threshold = 50
-local max_waste_threshold = 50
-local max_damage_threshold = 1
+local min_coolant_threshold = 0.5
+local max_heated_coolant_threshold = 0.5
+local max_waste_threshold = 0.5
+local max_damage_threshold = 0.1
 
 while true do
-	reactor_dat = poll_reactor(reactor)
+	local reactor_dat = poll_reactor(reactor)
 	
 	print("====")
-	if reactor_dat.coolant_filled_perc >= max_coolant_threshold then
-		print(string.format("Reactor coolant too high (%d%%), SCRAM!", reactor_dat.coolant_filled_perc))
+	print(os.date())
+	print("====")
+	if reactor_dat.coolant_filled_perc <= min_coolant_threshold then
+		print(string.format("Reactor coolant too high (%f%%), SCRAM!", reactor_dat.coolant_filled_perc))
 		reactor.scram()
 	else
-		print("Coolant nominal.")
+		print(string.format("Coolant nominal. %f%%", reactor_dat.coolant_filled_perc))
+	end
+	
+	if reactor_dat.heated_coolant_filled_perc >= max_heated_coolant_threshold then
+		print(string.format("Heated reactor coolant too high (%f%%), SCRAM!", reactor_dat.heated_coolant_filled_perc))
+		reactor.scram()
+	else
+		print(string.format("Heated coolant nominal. %f%%", reactor_dat.heated_coolant_filled_perc))
 	end
 	
 	if reactor_dat.waste_filled_perc >= max_waste_threshold then
-		print(string.format("Waste too high (%d%%), SCRAM!", reactor_dat.waste_filled_perc))
+		print(string.format("Waste too high (%f%%), SCRAM!", reactor_dat.waste_filled_perc))
 		reactor.scram()
 	else
-		print("Waste nominal.")
+		print(string.format("Waste nominal. %f%%", reactor_dat.waste_filled_perc))
 	end
 	
 	if reactor_dat.damage_percent >= max_damage_threshold then
-		print(string.format("Damage too high (%d%%), SCRAM!", reactor_dat.damage_percent))
+		print(string.format("Damage too high (%f%%), SCRAM!", reactor_dat.damage_percent))
 		reactor.scram()
 	else
-		print("Damage nominal.")
+		print(string.format("Damage nominal. %f%%", reactor_dat.damage_percent))
 	end
 	print("====")
 	
